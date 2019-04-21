d5cc74970761665b05dd3a809c0cbd9d77b39e16
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 20:33:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map {|name|
-    badge_maker(name)
-  end
+  attendees.map {|name| "Hello, my name is #{name}."
 end
 
 def assign_rooms(list)
