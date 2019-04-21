454cbb7bf7b814f31e65d6f08316b5390a790fe3
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 19:29:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,12 @@ def batch_badge_creator(attendees)
   end
 end
 
+def assign_rooms(list)
+  counter = 1
+  list.each do |speaker|
+    puts "Hello, #{speaker}! You'll be assigned to room #{counter}!"
+    counter+=1
+  end
+end
+
+
