daf76ba0313f532e563db9c1f3cf87e0b16b4334
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 19:04:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each{|name| puts "Hello, my name is #{name}."}
+  end
+end
+
